01c84d38697a2bbfdd0258b97c4fa97e3f66b454
gabrielsalazar8486 <salazargabriel8486@gmail.com>
2022-02-02 08:50:24 -0400

    Estados y ciclos de vida

---
+++ src/Clock.tsx
@@ -0,0 +1,5 @@
+const Clock = ({date}:{date:string})=>{
+    return (<h1>son las {date}</h1>);
+}
+
+export default Clock;
--- src/Welcome.tsx
+++
@@ -1,7 +0,0 @@
-const Welcome = ({name}:{ name:string })=>{
-    return (
-        <h1>Hola {name}</h1>
-    );
-}
-
-export default Welcome;
